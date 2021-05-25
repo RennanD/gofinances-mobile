f116f659a8c81ec09329f4bc311abaf744e5d212
rennand <rplaykl@gmail.com>
2021-05-24 22:12:07 -0300

    feat: create global theme

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Container } from './styles';
 
 export function Dashboard(): JSX.Element {
-  return <View />;
+  return <Container />;
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.background};
+  flex: 1;
+`;
 
 export const Title = styled.Text``;
