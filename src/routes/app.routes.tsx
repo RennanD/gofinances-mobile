d7eeb58199465a3badb33d9a8688f1dd5078c6f8
rennand <rplaykl@gmail.com>
2021-05-28 23:34:12 -0300

    feat: style tab bottom

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,70 @@
 import React from 'react';
+import { Platform, View } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { useTheme } from 'styled-components';
+
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes(): JSX.Element {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Dashboard" component={Dashboard} />
-      <Screen name="Register" component={Register} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.text,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 68,
+        },
+      }}
+    >
+      <Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          title: 'Listagem',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Register"
+        component={Register}
+        options={{
+          title: 'Cadastrar',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resume"
+        component={View}
+        options={{
+          title: 'Resumo',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="pie-chart-outlined"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
