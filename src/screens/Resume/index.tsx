ce2e6a31834f898a4d7e0a79a5979acc6457bbd3
rennand <rplaykl@gmail.com>
2021-05-31 22:57:23 -0300

    feat: listing expensives by category

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,15 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { Header } from '../../components/Header';
 import { HistoryCard } from '../../components/HistoryCard';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
+
+import { DataLitsProps } from '../Dashboard';
+import { categories } from '../../utils/categories';
+
+const dataKey = '@gofinances:transactions';
+
+interface CategoryResumeProps {
+  name: string;
+  amount: string;
+  color: string;
+}
 
 export function Resume(): JSX.Element {
+  const [categoriesResume, setCategoriesResume] =
+    useState<CategoryResumeProps[]>();
+
+  async function loadTransactions() {
+    const response = await AsyncStorage.getItem(dataKey);
+    const storegedTransactions: DataLitsProps[] = response
+      ? JSON.parse(response)
+      : [];
+    const expensives = storegedTransactions.filter(
+      expensive => expensive.type === 'outcome',
+    );
+
+    const totalByCategory: CategoryResumeProps[] = [];
+
+    categories.forEach(category => {
+      let categotySum = 0;
+
+      expensives.forEach(expensive => {
+        if (expensive.category === category.key) {
+          categotySum += Number(expensive.amount);
+        }
+      });
+
+      if (categotySum > 0) {
+        const amount = categotySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
+
+        totalByCategory.push({
+          name: category.name,
+          amount,
+          color: category.color,
+        });
+      }
+    });
+
+    setCategoriesResume(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <Container>
       <Header title="Resumo" />
 
-      <HistoryCard title="Compras" amount="R$ 150,50" color="red" />
+      <Content>
+        {categoriesResume?.map(category => (
+          <HistoryCard
+            key={category.name}
+            title={category.name}
+            amount={category.amount}
+            color={category.color}
+          />
+        ))}
+      </Content>
     </Container>
   );
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -3,3 +3,12 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+  },
+  showVerticalScrollIndicator: false,
+})`
+  flex: 1;
+`;
