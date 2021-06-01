977c4841d83ec83caef5355b1a4eaf39b7a8968e
rennand <rplaykl@gmail.com>
2021-05-31 23:26:41 -0300

    feat: redering pie chart with categories resume info

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
 
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { useTheme } from 'styled-components';
 import { Header } from '../../components/Header';
 import { HistoryCard } from '../../components/HistoryCard';
-
-import { Container, Content } from './styles';
-
 import { DataLitsProps } from '../Dashboard';
+
+import { ChartContainer, Container, Content } from './styles';
+
 import { categories } from '../../utils/categories';
 
 const dataKey = '@gofinances:transactions';
 
 interface CategoryResumeProps {
   name: string;
-  amount: string;
+  amount: number;
+  amountFormatted: string;
   color: string;
+  percent: number;
+  percentFormatted: string;
 }
 
 export function Resume(): JSX.Element {
   const [categoriesResume, setCategoriesResume] =
     useState<CategoryResumeProps[]>();
+
+  const theme = useTheme();
 
   async function loadTransactions() {
     const response = await AsyncStorage.getItem(dataKey);
@@ -29,6 +38,13 @@ export function Resume(): JSX.Element {
       : [];
     const expensives = storegedTransactions.filter(
       expensive => expensive.type === 'outcome',
+    );
+
+    const totalExpensives = expensives.reduce(
+      (accumulator: number, expensive: DataLitsProps) => {
+        return accumulator + Number(expensive.amount);
+      },
+      0,
     );
 
     const totalByCategory: CategoryResumeProps[] = [];
@@ -48,10 +64,15 @@ export function Resume(): JSX.Element {
           currency: 'BRL',
         });
 
+        const percent = (categotySum / totalExpensives) * 100;
+
         totalByCategory.push({
           name: category.name,
-          amount,
+          amount: categotySum,
+          amountFormatted: amount,
           color: category.color,
+          percent,
+          percentFormatted: `${percent.toFixed(0)}%`,
         });
       }
     });
@@ -68,11 +89,28 @@ export function Resume(): JSX.Element {
       <Header title="Resumo" />
 
       <Content>
+        <ChartContainer>
+          <VictoryPie
+            data={categoriesResume}
+            colorScale={categoriesResume?.map(category => category.color)}
+            style={{
+              labels: {
+                fontSize: RFValue(18),
+                fontWeight: 'bold',
+                fill: theme.colors.shape,
+              },
+            }}
+            labelRadius={60}
+            x="percentFormatted"
+            y="amount"
+          />
+        </ChartContainer>
+
         {categoriesResume?.map(category => (
           <HistoryCard
             key={category.name}
             title={category.name}
-            amount={category.amount}
+            amount={category.amountFormatted}
             color={category.color}
           />
         ))}
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -12,3 +12,8 @@ export const Content = styled.ScrollView.attrs({
 })`
   flex: 1;
 `;
+
+export const ChartContainer = styled.View`
+  width: 100%;
+  align-items: center;
+`;
